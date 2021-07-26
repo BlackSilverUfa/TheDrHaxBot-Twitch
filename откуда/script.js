b959310e6f8eaf3ce6df9305a8d44784db576704
Node-RED <the.dr.hax@gmail.com>
2021-07-26 23:27:36 +0300

    Automated update

--- откуда/script.js
+++ откуда/script.js
@@ -1,34 +1,3 @@
-function get(context, key, store_name) {
-    return new Promise((resolve, reject) => {
-        context.get(key, store_name, (err, value) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(value);
-            }
-        });
-    });
-}
-
-function set(context, key, value, store_name) {
-    return new Promise((resolve, reject) => {
-        context.set(key, value, store_name, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
-}
-
-if (msg.init) {
-    set(context, 'answers', msg.payload, 'memory');
-    return;
-}
-
-// -----------------------------------------
-
 function tokenize(string) {
     string = string.toLowerCase().replace(/ё/g, 'е').trim();
     return string.split(' ').map((word) => {
@@ -37,97 +6,109 @@ function tokenize(string) {
     }).filter((x) => x != null);
 }
 
-function fts(query, items, lambda) {
-    query = tokenize(query);
-    let maxRank = 0;
-
-    return items
-        .map((item) => {
-            const words = tokenize(lambda(item));
-
-            const rank = query.map((queryWord) => (
-                words.filter((word) => (
-                    word.startsWith(queryWord) &&
-                    word.length / 2 <= queryWord.length
-                )).length > 0
-            )).reduce((a, b) => a + b);
-
-            if (rank > maxRank) {
-                maxRank = rank;
-            }
-
-        return { item, rank };
-    })
-    .filter((item) => item.rank === maxRank && item.rank > 0)
-    .map((item) => item.item);
+function strip(string) {
+    return tokenize(string).join(' ');
 }
 
 function choose(list) {
     return list[Math.floor(Math.random() * list.length)]
 }
 
-async function main() {
-    let query = msg.parsed.query_filtered;
-    const answers = await get(context, 'answers', 'memory');
+if (msg.init) {
+    lunrStemmer(lunr);
+    lunrRu(lunr);
+    lunrMulti(lunr);
 
-    if (tokenize(query).length == 0) {
-        let examples = [];
-    
-        while (examples.length < 10) {
-            let x = choose(Object.keys(answers));
+    const index = lunr(function() {
+        this.use(lunr.multiLanguage('en', 'ru'));
+        this.field('query');
+        this.ref('id');
 
-            if (x.indexOf('/') !== -1) {
-                x = choose(x.split('/'));
-            }
+        Object.entries(msg.payload).forEach(([key, answer]) => {
+            this.add({
+                id: key,
+                query: strip(key)
+            });
+        });
+    });
 
-            if (examples.indexOf(x) === -1) {
-                examples.push(x);
-            }
+    node.status(`${Object.keys(msg.payload).length} ответов`);
+
+    context.set('answers', msg.payload, 'memory');
+    context.set('index', index, 'memory');
+    return;
+}
+
+const [answers, index] = context.get(['answers', 'index'], 'memory');
+
+let query = strip(msg.parsed.query_filtered);
+
+if (query.length == 0) {
+    const examples = [];
+
+    while (examples.length < 10) {
+        let x = choose(Object.keys(answers));
+
+        if (x.indexOf(' / ') !== -1) {
+            x = choose(x.split(' / '));
         }
 
-        msg.key = null;
-        msg.reply = 'примеры запросов: ' + examples.join(' | ');
-        return msg;
-    }
-
-    for (let [key, answer] of Object.entries(answers)) {
-        if (typeof(answer) == 'object' && answer.regex) {
-            let regex = new RegExp(answer.regex, 'ig');
-
-            if (query.match(regex)) {
-                query = query.replace(regex, key);
-            }
-        }
-    }
-    
-    let results = fts(query, Object.keys(answers), (x) => x);
-
-    if (results.length === 0) {
-        msg.key = null;
-        msg.reply = "пока не знаю KEKWait";
-    } else {
-        let key = results[0];
-        let answer = answers[key];
-
-        if (typeof(answer) == 'object') {
-            if (answer.items) {
-                msg.key = null;
-                msg.reply = answer.prefix + choose(answer.items);
-                
-                if (answer.suffix) {
-                    msg.reply += answer.suffix;
-                }
-            } else if (answer.answer) {
-                msg.key = key;
-                msg.reply = answer.answer;
-            }
-        } else {
-            msg.key = key;
-            msg.reply = answer;
+        if (examples.indexOf(x) === -1) {
+            examples.push(x);
         }
     }
 
+    msg.key = null;
+    msg.reply = 'примеры запросов: ' + examples.join(' | ');
     return msg;
 }
 
-return main();
+Object.entries(answers)
+    .filter(([k, a]) => typeof(a) == 'object' && a.regex)
+    .forEach(([k, a]) => {
+        let regex = new RegExp(a.regex, 'ig');
+
+        if (query.match(regex)) {
+            query = query.replace(regex, k);
+        }
+    });
+
+let max_rank = 0;
+
+const results = index.search(query).map((x) => {
+    const keywords = Object.keys(x.matchData.metadata);
+    const rank = keywords.length;
+
+    if (rank > max_rank) {
+        max_rank = rank;
+    }
+
+    return { ref: x.ref, rank };
+}).filter((x) => x.rank == max_rank).map((x) => x.ref);
+
+if (results.length === 0) {
+    msg.key = null;
+    msg.reply = "пока не знаю KEKWait";
+} else {
+    const key = results[0];
+    const answer = answers[key];
+
+    if (typeof(answer) == 'object') {
+        if (answer.items) {
+            msg.key = null;
+            msg.reply = answer.prefix + choose(answer.items);
+            
+            if (answer.suffix) {
+                msg.reply += answer.suffix;
+            }
+        } else if (answer.answer) {
+            msg.key = key;
+            msg.reply = answer.answer;
+        }
+    } else {
+        msg.key = key;
+        msg.reply = answer;
+    }
+}
+
+return msg;
