ed5d283f87bfe79f422019014e3382e0a7962906
Node-RED <the.dr.hax@gmail.com>
2021-07-26 18:05:20 +0300

    Automated update

--- откуда/script.js
+++ откуда/script.js
@@ -29,15 +29,34 @@ if (msg.init) {
 
 // -----------------------------------------
 
-const elasticlunr = require('elasticlunr@0.9.5');
-const translit = require('cyrillic-to-translit-js@3.1.0')();
+function tokenize(string) {
+    string = string.toLowerCase().replace(/ё/g, 'е').trim();
+    return string.split(' ').map((word) => {
+        const match = word.match(/[a-zа-я0-9]+/g);
+        return match ? match.join('') : null;
+    }).filter((x) => x != null);
+}
 
-function strip(string) {
-    string = string.toLowerCase().replace(/ё/g, 'е');
-    return translit.transform(string).trim().split(' ').map((word) => {
-        let match = word.match(/[a-zа-я0-9]+/g);
-        return match ? match.join('') : '';
-    }).join(' ');
+function fts(query, items, lambda) {
+    query = tokenize(query);
+    let maxRank = 0;
+
+    return items
+        .map((item) => {
+            const words = tokenize(lambda(item));
+
+            const rank = query.map((queryWord) => (
+                words.filter((word) => word.startsWith(queryWord)).length > 0
+            )).reduce((a, b) => a + b);
+
+            if (rank > maxRank) {
+                maxRank = rank;
+            }
+
+        return { item, rank };
+    })
+    .filter((item) => item.rank === maxRank && item.rank > 0)
+    .map((item) => item.item);
 }
 
 function choose(list) {
@@ -48,7 +67,7 @@ async function main() {
     let query = msg.parsed.query_filtered;
     const answers = await get(context, 'answers', 'memory');
 
-    if (strip(query).length == 0) {
+    if (tokenize(query).length == 0) {
         let examples = [];
     
         while (examples.length < 10) {
@@ -68,15 +87,6 @@ async function main() {
         return msg;
     }
 
-    elasticlunr.clearStopWords();
-    elasticlunr.addStopWords([
-        'of', 'the', 'я', 'и', 'с', 'на', 'не', 'из', 'о', 'за', 'в'
-    ].map((s) => strip(s)));
-    
-    const index = elasticlunr();
-    index.addField('query');
-    index.setRef('id');
-
     for (let [key, answer] of Object.entries(answers)) {
         if (typeof(answer) == 'object' && answer.regex) {
             let regex = new RegExp(answer.regex, 'ig');
@@ -85,22 +95,15 @@ async function main() {
                 query = query.replace(regex, key);
             }
         }
-
-        index.addDoc({
-            id: key,
-            query: strip(key)
-        });
     }
     
-    let results = index.search(strip(query));
-    
+    let results = fts(query, Object.keys(answers), (x) => x);
+
     if (results.length === 0) {
         msg.key = null;
         msg.reply = "пока не знаю KEKWait";
     } else {
-        msg.score = results[0].score;
-        
-        let key = results[0].ref;
+        let key = results[0];
         let answer = answers[key];
 
         if (typeof(answer) == 'object') {
