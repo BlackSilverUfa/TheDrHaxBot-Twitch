69f4d670e1269f01c80906f8772802d48249c54f
Node-RED <the.dr.hax@gmail.com>
2021-12-23 19:00:01 +0300

    Automated update

--- common.js
+++ common.js
@@ -17,33 +17,34 @@ function agreeWithNum(num, words) {
 
 // https://stackoverflow.com/a/32180863
 function msToDelta(ms) {
-    let seconds = (ms / 1000).toFixed(1);
-    let minutes = (ms / (1000 * 60)).toFixed(1);
-    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
-    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-    
+    let seconds = (ms / 1000).toFixed(1),
+        minutes = (ms / (1000 * 60)).toFixed(1),
+        hours = (ms / (1000 * 60 * 60)).toFixed(1),
+        days = (ms / (1000 * 60 * 60 * 24)).toFixed(1),
+        weeks = (ms / (1000 * 60 * 60 * 24 * 7)).toFixed(1);
+
     if (seconds < 60)
         return seconds + ' ' + agreeWithNum(seconds, ['секунда', 'секунды', 'секунд']);
     else if (minutes < 60)
         return minutes + ' ' + agreeWithNum(minutes, ['минута', 'минуты', 'минут']);
     else if (hours < 24)
         return hours + ' ' + agreeWithNum(hours, ['час', 'часа', 'часов']);
-    else
+    else if (days < 7)
         return days + ' ' + agreeWithNum(days, ['день', 'дня', 'дней']);
+    else
+        return weeks + ' ' + agreeWithNum(weeks, ['неделя', 'недели', 'недель']);
 }
 
 // https://stackoverflow.com/a/19700358
 function msToTime(duration) {
-    let milliseconds = Math.floor((duration % 1000) / 100),
-        seconds = Math.floor((duration / 1000) % 60),
+    let seconds = Math.floor((duration / 1000) % 60),
         minutes = Math.floor((duration / (1000 * 60)) % 60),
         hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-    // hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-  
-    return hours + ":" + minutes + ":" + seconds;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 flow.set('func', {
