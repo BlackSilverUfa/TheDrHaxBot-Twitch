fc3da7a0ba1889227608c6f3effe2f95f84abee4
Node-RED <the.dr.hax@gmail.com>
2021-12-23 16:00:02 +0300

    Automated update

--- время.js
+++ время.js
@@ -1,30 +1,49 @@
 const stream = flow.get('stream_status', 'file'); // https://red.thedrhax.pw/blackufa/twitch
 const rerun = flow.get('rerun_status', 'file');
 
+function agreeWithNum(num, words) {
+    if (num >= 20) {
+        num = num % 10;
+    }
+
+    if (num == 1) {
+        return words[0];
+    } else if (num > 1 && num < 5) {
+        return words[1];
+    } else {
+        return words[2];
+    }
+}
+
 // https://stackoverflow.com/a/32180863
 function msToDelta(ms) {
-  let seconds = (ms / 1000).toFixed(1);
-  let minutes = (ms / (1000 * 60)).toFixed(1);
-  let hours = (ms / (1000 * 60 * 60)).toFixed(1);
-  let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-  if (seconds < 60) return seconds + " сек.";
-  else if (minutes < 60) return minutes + " мин.";
-  else if (hours < 24) return hours + " ч.";
-  else return days + " дн.";
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    
+    if (seconds < 60)
+        return seconds + ' ' + agreeWithNum(seconds, ['секунда', 'секунды', 'секунд']);
+    else if (minutes < 60)
+        return minutes + ' ' + agreeWithNum(minutes, ['минута', 'минуты', 'минут']);
+    else if (hours < 24)
+        return hours + ' ' + agreeWithNum(hours, ['час', 'часа', 'часов']);
+    else
+        return days + ' ' + agreeWithNum(days, ['день', 'дня', 'дней']);
 }
 
 // https://stackoverflow.com/a/19700358
 function msToTime(duration) {
-  let milliseconds = Math.floor((duration % 1000) / 100),
-      seconds = Math.floor((duration / 1000) % 60),
-      minutes = Math.floor((duration / (1000 * 60)) % 60),
-      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    let milliseconds = Math.floor((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  // hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-  return hours + ":" + minutes + ":" + seconds;
+    // hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    return hours + ":" + minutes + ":" + seconds;
 }
 
 if (stream.active) {
--- чобыло.js
+++ чобыло.js
@@ -1,15 +1,34 @@
 const stream = flow.get('stream_status', 'file'); // https://red.thedrhax.pw/blackufa/twitch
 
+function agreeWithNum(num, words) {
+    if (num >= 20) {
+        num = num % 10;
+    }
+
+    if (num == 1) {
+        return words[0];
+    } else if (num > 1 && num < 5) {
+        return words[1];
+    } else {
+        return words[2];
+    }
+}
+
 // https://stackoverflow.com/a/32180863
 function msToDelta(ms) {
-  let seconds = (ms / 1000).toFixed(1);
-  let minutes = (ms / (1000 * 60)).toFixed(1);
-  let hours = (ms / (1000 * 60 * 60)).toFixed(1);
-  let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-  if (seconds < 60) return seconds + " сек.";
-  else if (minutes < 60) return minutes + " мин.";
-  else if (hours < 24) return hours + " ч.";
-  else return days + " дн.";
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    
+    if (seconds < 60)
+        return seconds + ' ' + agreeWithNum(seconds, ['секунда', 'секунды', 'секунд']);
+    else if (minutes < 60)
+        return minutes + ' ' + agreeWithNum(minutes, ['минута', 'минуты', 'минут']);
+    else if (hours < 24)
+        return hours + ' ' + agreeWithNum(hours, ['час', 'часа', 'часов']);
+    else
+        return days + ' ' + agreeWithNum(days, ['день', 'дня', 'дней']);
 }
 
 let timeline = stream.game_history.map((game, i, arr) => [
