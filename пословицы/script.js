417b90712117fb2e0a60376f179b2f35042fe08b
Node-RED <the.dr.hax@gmail.com>
2022-01-11 03:02:08 +0300

    Automated update

--- пословицы/script.js
+++ пословицы/script.js
@@ -1,8 +1,24 @@
-const { choose } = flow.get('func', 'memory');
+if (msg.init) {
+    const data = {...msg.payload};
 
-msg.reply = choose(msg.answers.split('\n'));
+    data.words = Object.assign({},
+        ...data.words.map(w => ({
+            [w.forms.nomn]: w
+        }))
+    );
 
-if (msg.parsed.command.toLowerCase() == 'поползень') {
+    context.set('data', data, 'memory');
+    node.status('Ready');
+    return;
+}
+
+const { choose, renderTemplate } = flow.get('func', 'memory');
+const { words, proverbs } = context.get('data', 'memory');
+
+const word = words[msg.parsed.icommand] || choose(Object.values(words));
+msg.reply = renderTemplate(choose([...proverbs, ...word.specials]), word.forms);
+
+if (msg.parsed.icommand == 'поползень') {
     msg.reply += ' popCat';
 } else {
     msg.reply += ' ' + choose([
