8831ba2c4aa0e6e2bb34b885d10fb44efb417b5b
Node-RED <the.dr.hax@gmail.com>
2022-10-26 03:02:36 +0300

    Automated update

--- откуда/script.js
+++ откуда/script.js
@@ -1,32 +1,17 @@
-const { tokenize, renderTemplate } = flow.get('func', 'memory');
-
-function strip(string) {
-    return tokenize(string).join(' ');
-}
-
-function choose(list) {
-    return list[Math.floor(Math.random() * list.length)]
-}
+const { tokenize, choose, renderTemplate } = flow.get('func', 'memory');
 
 if (msg.init) {
-    lunrStemmer(lunr);
-    lunrRu(lunr);
-    lunrMulti(lunr);
-
-    const index = lunr(function() {
-        this.use(lunr.multiLanguage('en', 'ru'));
-        this.pipeline.remove(lunr.stopWordFilter);
-        this.pipeline.remove(lunr.ru.stopWordFilter);
-        this.field('query');
-        this.ref('id');
-
-        Object.entries(msg.payload).forEach(([key, answer]) => {
-            this.add({
-                id: key,
-                query: strip(key)
-            });
-        });
+    const index = new MiniSearch({
+        fields: ['id'],
+        searchOptions: {
+            prefix: true,
+            fuzzy: 0,
+            combineWith: 'AND',
+        },
+        tokenize,
     });
+
+    index.addAll(Object.keys(msg.payload).map((key) => ({ id: key })));
 
     node.status(`${Object.keys(msg.payload).length} ответов`);
 
@@ -37,7 +22,7 @@ if (msg.init) {
 
 const [answers, index] = context.get(['answers', 'index'], 'memory');
 
-let query = strip(msg.parsed.query_filtered);
+let query = msg.parsed.query_filtered;
 
 if (query.length == 0) {
     const examples = [];
@@ -69,25 +54,10 @@ Object.entries(answers)
         }
     });
 
-let max_rank = 0;
+const [result] = index.search(query);
 
-const results = index.search(query).map((x) => {
-    const keywords = Object.keys(x.matchData.metadata);
-    const rank = keywords.length;
-
-    if (rank > max_rank) {
-        max_rank = rank;
-    }
-
-    return { ref: x.ref, rank };
-}).filter((x) => x.rank == max_rank).map((x) => x.ref);
-
-if (results.length === 0) {
-    msg.key = null;
-    msg.reply = "пока не знаю KEKWait";
-} else {
-    const key = results[0];
-    const answer = answers[key];
+if (result != null) {
+    const answer = answers[result.id];
 
     if (typeof(answer) == 'object') {
         if (answer.items) {
@@ -102,13 +72,16 @@ if (results.length === 0) {
                 });
             }
         } else if (answer.answer) {
-            msg.key = key;
+            msg.key = result.id;
             msg.reply = answer.answer;
         }
     } else {
-        msg.key = key;
+        msg.key = result.id;
         msg.reply = answer;
     }
+} else {
+    msg.key = null;
+    msg.reply = "пока не знаю KEKWait";
 }
 
 return msg;
