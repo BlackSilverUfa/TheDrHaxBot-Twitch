446b322d1bb218d55c7a3a8324ed23d9093976c7
Node-RED <the.dr.hax@gmail.com>
2022-10-31 17:00:02 +0300

    Automated update

--- время.js
+++ время.js
@@ -15,18 +15,18 @@ if (stream.active) {
     const total = dateDistance(new Date(stream.date), null, DATE_DIST_OPTS);
 
     if (stream.game_history.length <= 1) {
-        msg.reply = `стрим идёт уже ${total} (c ${stream.time} МСК) YEPPERS`;
+        msg.reply = `стрим идёт уже ${total} (c ${stream.time} МСК)`;
     } else {
         const lastCategory = stream.game_history[stream.game_history.length - 1];
         const catAge = dateDistance(new Date(lastCategory.date), null, DATE_DIST_OPTS);
 
-        msg.reply = `стрим идёт уже ${total} (c ${stream.time} МСК), а эта игра - ${catAge} YEPPERS`;
+        msg.reply = `стрим идёт уже ${total} (c ${stream.time} МСК), а эта игра - ${catAge}`;
     }
 } else {
     const total = dateDistance(new Date(stream.date), new Date(stream.date_end), DATE_DIST_OPTS);
     const when = dateDistance(new Date(stream.date_end), null, { short: true, });
 
-    msg.reply = `стрим шёл ${total} и закончился ${when} назад peepoSHAKE`;
+    msg.reply = `стрим шёл ${total} и закончился ${when} назад`;
 }
 
 return msg;
