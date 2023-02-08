428b7ce3f40f574c1acdd4180b30e277c4aeb39d
Node-RED <the.dr.hax@gmail.com>
2023-02-09 00:00:08 +0300

    Automated update

--- icq/script.js
+++ icq/script.js
@@ -30,6 +30,16 @@ function rand(min, max) {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
 
+function emote(icq) {
+    const groupData = settings.groups[icq.group];
+
+    if (Object.keys(groupData.special).indexOf(`${icq.value}`) !== -1) {
+        return groupData.special[`${icq.value}`];
+    }
+
+    return groupData.emotes[Math.floor(icq.value / groupData.step)];
+}
+
 async function main() {
     const _id = msg.payload.userstate.username;
     const [cmd, ...args] = msg.parsed.query_filtered.split(' ');
@@ -37,9 +47,90 @@ async function main() {
 
     switch (cmd) {
         case 'help':
-            msg.reply = 'доступные команды: lock, unlock, help';
+            msg.reply = 'доступные команды: lock, unlock, help, swap';
             return msg;
-        
+
+        case 'swap':
+            if (msg.parsed.mentions_list.length === 0) {
+                msg.reply = 'укажите ник получателя через @';
+                return msg;
+            }
+
+            const [rName] = msg.parsed.mentions_list;
+            const [oIcq] = await amongo(DB, 'find', { transfer: _id });
+
+            if (oIcq) {
+                msg.reply = `вы уже предложили обмен @${oIcq._id}. Используйте !icq cancel, чтобы отменить YEPPERS`;
+                return msg;
+            }
+
+            const [rIcq] = await amongo(DB, 'find', { _id: rName });
+
+            if (!rIcq) {
+                msg.reply = `получатель не найден peepoThink`;
+                return msg;
+            }
+
+            if (rIcq.transfer) {
+                msg.reply = `получателю уже поступил запрос от ${rIcq.transfer}`;
+                return msg;
+            }
+
+            rIcq.transfer = _id;
+            msg.reply = `@${_id} предлагает @${rName} поменяться ICQ: ${icq.value} ${emote(icq)} ↔ ${rIcq.value} ${emote(rIcq)}`
+            msg.reply += ' Используйте !icq accept/decline, чтобы принять/отклонить YEPPERS';
+
+            await amongo(DB, 'save', rIcq);
+            return msg;
+
+        case 'cancel':
+            const [cIcq] = await amongo(DB, 'find', { transfer: _id });
+
+            if (!cIcq) {
+                msg.reply = 'активных предложений не найдено peepoThink';
+                return msg;
+            }
+
+            delete cIcq.transfer;
+            await amongo(DB, 'save', cIcq);
+
+            msg.reply = 'предложение успешно отменено SeemsGood';
+            return msg;
+
+        case 'accept':
+            if (!icq.transfer) {
+                msg.reply = 'нет активных предложений peepoThink';
+                return msg;
+            }
+
+            const [sIcq] = await amongo(DB, 'find', { _id: icq.transfer });
+
+            [sIcq.value, icq.value] = [icq.value, sIcq.value];
+            [sIcq.group, icq.group] = [icq.group, sIcq.group];
+            [sIcq.lock, icq.lock] = [icq.lock, sIcq.lock];
+            delete icq.transfer;
+
+            await Promise.all([
+                amongo(DB, 'save', icq),
+                amongo(DB, 'save', sIcq),
+            ]);
+
+            msg.reply = 'обмен успешно завёршён! Используйте "!icq?" для проверки или сразу "!icq lock" для блокировки peepoHappier';
+            return msg;
+
+        case 'decline':
+            if (!icq.transfer) {
+                msg.reply = 'нет активных предложений peepoThink';
+                return msg;
+            }
+
+            const sName = icq.transfer;
+            delete icq.transfer;
+            await amongo(DB, 'save', icq);
+
+            msg.reply = `предложение об обмене ICQ с ${sName} отклонено`;
+            return msg;
+
         case 'lock':
             if (!icq) {
                 msg.reply = 'вы ещё не проверяли ICQ BUFANerd';
@@ -113,16 +204,8 @@ async function main() {
     const groupData = settings.groups[group];
     let template = groupData.templates[delta !== 0 ? 'delta' : 'simple'];
 
-    let emote;
-
-    if (Object.keys(groupData.special).indexOf(`${value}`) !== -1) {
-        emote = groupData.special[`${value}`];
-
-        if (delta != 0) {
-            template += ' Вы можете закрепить это значение командой "!icq lock" YEPPERS';
-        }
-    } else {
-        emote = groupData.emotes[Math.floor(value / groupData.step)];
+    if (Object.keys(groupData.special).indexOf(`${value}`) !== -1 && delta != 0) {
+        template += ' Вы можете закрепить это значение командой "!icq lock" YEPPERS';
     }
 
     return {
@@ -130,7 +213,7 @@ async function main() {
         reply: renderTemplate(template, {
             value,
             delta: delta > 0 ? `+${delta}` : delta,
-            emote
+            emote: emote({ value, group }),
         })
     };
 }
