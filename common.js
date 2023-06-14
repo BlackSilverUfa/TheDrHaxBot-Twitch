086f185d6e1dce7a1dbc400e89ad9bd659d80b4f
Node-RED <the.dr.hax@gmail.com>
2023-06-14 05:00:06 +0300

    Automated update

--- common.js
+++ common.js
@@ -228,11 +228,17 @@ const ptime = (t) => {
 };
 
 const ftime = (t) => (
-    range(2, -1, -1).map((i) => {
-        const res = Math.floor(t / 60 ** i);
-        t %= 60 ** i;
-        return padStart(res, 2, '0');
-    }).join(':')
+    range(2, -1, -1).reduce((acc, cur) => {
+        const res = Math.floor(t / 60 ** cur);
+        if (cur === 2 && res === 0) return acc; // skip 0 hours
+        t %= 60 ** cur;
+        return [
+            ...acc,
+            acc.length > 0
+                ? padStart(res, 2, '0')
+                : res
+        ];
+    }, []).join(':')
 );
 
 const getStreamInfo = (channel) => {
