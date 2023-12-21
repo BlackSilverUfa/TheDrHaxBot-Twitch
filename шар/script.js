1536432da5ced37b86dce955fd0f257886c8bee8
Node-RED <the.dr.hax@gmail.com>
2023-12-21 15:00:08 +0300

    Automated update

--- шар/script.js
+++ шар/script.js
@@ -225,7 +225,7 @@ const FIXED_ANSWERS = {
         'пахсив YEPPERS',
         'мелюзина NOPERS',
     ]),
-    '^oratrice': () => 'Jammies Mecanique BSUJam d\'Analyse kiryuJAM Cardinale BSUFlex',
+    '^oratrice': () => 'Jammies Mecanique BSUJam d\'Analyse kiryuJAM Cardinale OUIP',
 
     '^omae wa mou shindeiru': () => 'nani?! BSURage',
     '^osmanthus': () => '...wine tastes the same as I remember KEKLEO',
