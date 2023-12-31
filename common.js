f41b1d49d2b9ceb42cfb3548889bab09d2059920
Node-RED <the.dr.hax@gmail.com>
2024-01-01 00:00:11 +0300

    Automated update

--- common.js
+++ common.js
@@ -12,7 +12,7 @@ function mulberry32(a) {
         var t = a += 0x6D2B79F5;
         t = Math.imul(t ^ t >>> 15, t | 1);
         t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-        return 1 - ((t ^ t >>> 14) >>> 0) / 4294967296;
+        return ((t ^ t >>> 14) >>> 0) / 4294967296;
     }
 }
 
