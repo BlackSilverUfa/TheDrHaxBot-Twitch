2a79138829ff23a4dae74ba4bb0179c1b5b3cd91
Node-RED <the.dr.hax@gmail.com>
2024-12-09 19:00:04 +0300

    Automated update

--- кусь/script.js
+++ кусь/script.js
@@ -21,7 +21,7 @@ function bite(user) {
         case '<@692736268567707719>':
         case 'kovalenko_p':
         case 'tomatepotato':
-            if (random() < 0.25) break;
+            if (random() > 0.25) break;
 
             return choose([
                 'потат Tastge 🥔',
